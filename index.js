b63b8116e0d97044b28d21f6629ac0b5b2b02b79
Heri Roque <heriroque.22@gmail.com>
2023-05-02 21:53:12 -0500

    add: events handlers config archives

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 const { token } = require('./config.json');
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
 
 // Create a new client instance
-const client = new Client({
-	intents: [3276799],
+const client = new Client({ 
+  intents: [Guilds, GuildMembers, GuildMessages],
+  Partials: [User, Message, GuildMember, ThreadMember] 
 });
 
+const { loadEvents } = require("./Handlers/eventHandler");
+
+client.events = new Collection();
+
+loadEvents(client);
+
 // Log in to Discord with your client's token
-client
-	.login(token)
-	.then(() => {
-		console.log(`Bot ${client.user.tag} se ha iniciado`);
-		client.user.setActivity(`Test`);
-	})
-	.catch((err) => console.log(err));
+client.login(token);
+
+// .then(() => {
+// 	console.log(`Bot ${client.user.tag} se ha iniciado`);
+// 	client.user.setActivity(``)
+// })
+// .catch((err) => console.log(err));
